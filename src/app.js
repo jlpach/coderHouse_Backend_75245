a9fcd5b498e301da079a87f6f64b6d22acac9464
jlpach <jlpach@gmail.com>
2025-03-14 09:30:23 -0600

    New handlebars changes added

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 const express = require("express")
 const productsRouter = require('./routes/productos.route.js')
 const cartsRouter = require('./routes/carts.route.js')
+const viewsRouter = require('./routes/views.router.js')
+const { engine } = require("express-handlebars")
 
 const PORT = 8080
 
@@ -9,8 +11,13 @@ const app = express()
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
 
+app.engine("handlebars", engine())
+app.set("view engine", "handlebars")
+app.set("views", "../src/views")
+
 app.use('/api/products', productsRouter)
 app.use('/api/carts', cartsRouter)
+app.use('/', viewsRouter)
 
 app.get("/", (req, res) => {
     res.setHeader('Content-Type', 'Text/plain')
---
+++ src/routes/views.router.js
@@ -0,0 +1,14 @@
+const { Router } = require("express")
+const { ProductsManager } = require("../dao/ProductsManager.js")
+const router = Router()
+
+const productsManager = new ProductsManager("./src/data/products.json");
+
+router.get("/realtimeproducts", async (req, res) => {
+    let productos = await productsManager.getProducts
+    let producto = productos[2]
+
+    res.render("realtimeproducts", { titulo: producto.title })
+})
+
+module.exports = router
